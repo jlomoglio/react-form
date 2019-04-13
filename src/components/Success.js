53edecde7629f54baca067497f98e28f3b4fa37f
Joe LoMoglio <joe.lomoglio@gmail.com>
2019-04-12 22:23:17 -0400

    Initial commit

---
+++ src/components/Success.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+class Success extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Success" />
+          <h1>Thank You For Your Submission</h1>
+          <p>
+            You will get an email with detailed instructions.
+          </p>
+        </React.Fragment>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default Success;
---
+++ src/components/UserForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
+
+class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: ''
+  };
+
+  // Proceed to next step
+  nextStep = () => {
+    const { step } = this.state
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  // Go back to previous step
+  prevStep = () => {
+    const { step } = this.state
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change (curried function is a function that returns a function)
+  // [input] is a computed property name, It's a shorthand for the someObject[someKey] assignment. 
+  handleChange = (input) => (e) => {
+    this.setState({
+      [input]: e.target.value
+    });
+  };
+
+  render() {
+    // Destructuring
+    const { step, firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { step, firstName, lastName, email, occupation, city, bio };
+
+    switch(step) {
+      case 1:
+        return (
+          <FormUserDetails 
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4  :
+        return <Success />;
+    };
+  };
+};
+
+export default UserForm;
